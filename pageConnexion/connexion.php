ad9164877989c1016d66ad378d032ec877b39ea6
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-02-06 16:55:13 +0100

    Modification page connexion

--- pageConnexion/connexion.php
+++ pageConnexion/connexion.php
@@ -1,67 +1,15 @@
-<?php
-session_start();
-/**
- * @file connexion.php 
- * @brief Page implementant la connexion des utilisateurs
- * @autor Guillaume Arricastre
- * version 
- * date 
- * 
- * */
-
-$login_valide_admin = "root";
-$pwd_valide_admin = "root";
-
-
-//si le bouton "Connexion" est cliqué
-if(isset($_POST['connexion'])){
-    // on vérifie que le champ "Pseudo" n'est pas vide
-    // empty vérifie à la fois si le champ est vide et si le champ existe belle et bien
-    if(empty($_POST['pseudo'])){
-        echo "Le champ Pseudo est vide.";
-    } else {
-        // on vérifie maintenant si le champ "Mot de passe" n'est pas vide"
-        if(empty($_POST['mdp'])){
-            echo "Le champ Mot de passe est vide.";
-        } else {
-            
-            $Pseudo =($_POST['pseudo']); 
-            $MotDePasse =($_POST['mdp']);
-            //on se connecte à la base de données:
-
-            //on vérifie que la connexion s'effectue correctement:
-            //on fait maintenant la requête dans la base de données pour rechercher si ces données existent et correspondent:
-                
-                if ($login_valide_admin == $_POST['pseudo'] && $pwd_valide_admin == $_POST['mdp']) {
-
-                 $_SESSION['pseudo'] = $_POST['pseudo'];
-                 $_SESSION['mdp'] = $_POST['mdp'];
-                 $CONNEXION=true;
-
-              
-                header ('location: ../index.php');
-         
-                    
-            }
-            else {
-                echo "Mauvais identifiants fournies";
-            }
-        }
-    }
-}
-
-?>
+<!DOCTYPE html>
 <html style="font-size: 16px;" lang="fr"><head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <meta name="keywords" content="">
     <meta name="description" content="">
-    <title>Connexion</title>
-    <link rel="stylesheet" href="nicepageConnexion.css" media="screen">
+    <title>Page 2</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
 <link rel="stylesheet" href="Page-2.css" media="screen">
-    <script class="u-script" type="text/javascript" src="jquery-1.9.1.minConnexion.js" defer=""></script>
-    <script class="u-script" type="text/javascript" src="nicepageConnexion.js" defer=""></script>
-    <meta name="generator" content="Nicepage 5.4.4, nicepage.com">
+    <script class="u-script" type="text/javascript" src="jquery-1.9.1.min.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 5.4.6, nicepage.com">
     <meta name="referrer" content="origin">
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
     
@@ -77,26 +25,35 @@ if(isset($_POST['connexion'])){
     <meta property="og:description" content="">
     <meta property="og:type" content="website">
     <link rel="canonical" href="/">
-    <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
-  <body data-home-page="https://website3965907.nicepage.io/Page-2.html?version=125cd024-bea8-4711-b237-1d84130f05eb" data-home-page-title="Page 2" class="u-body u-xl-mode" data-lang="fr">
+  <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
+  <body data-home-page="https://website3965907.nicepage.io/Page-2.html?version=cf4b8671-525a-4d30-8372-ab1945778d4d" data-home-page-title="Page 2" class="u-body u-xl-mode" data-lang="fr">
     <section class="u-align-left u-clearfix u-image u-shading u-section-1" src="" data-image-width="1503" data-image-height="1000" id="sec-864e">
       <div class="u-clearfix u-sheet u-sheet-1">
         <h1 class="u-text u-text-default u-title u-text-1">connexion</h1>
         <div class="u-form u-form-1">
-          <form action="connexion.php"  style="padding: 10px;" method="post">
+          <form action="https://forms.nicepagesrv.com/v2/form/process" class="u-clearfix u-form-spacing-10 u-form-vertical u-inner-form" source="email" name="form" style="padding: 10px;">
             <div class="u-form-group u-form-name">
               <label for="name-1eed" class="u-label u-label-1">LOGIN</label>
-              <input type="text" placeholder="Saisir votre login" id="login" name="pseudo" class="u-input u-input-rectangle u-radius-46 u-white u-input-1" required="">
+              <input type="text" placeholder="Saisir votre login" id="name-1eed" name="name" class="u-input u-input-rectangle u-radius-46 u-white u-input-1" required="">
             </div>
             <div class="u-form-group">
               <label for="email-1eed" class="u-label u-label-2">MOT DE PASSE</label>
-              <input type="password" placeholder="Saisir votre mot de passe" id="password" name="mdp" class="u-input u-input-rectangle u-radius-46 u-white u-input-2" required="required">
+              <input placeholder="Saisir votre mot de passe" id="email-1eed" name="email" class="u-input u-input-rectangle u-radius-46 u-white u-input-2" required="required" type="text">
             </div>
-            <button type="submit" name="connexion" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-2">CONNEXION</button>
+            <div class="u-align-left u-form-group u-form-submit">
+              <a href="#" class="u-btn u-btn-submit u-button-style u-hover-palette-1-dark-1 u-palette-1-base u-btn-1" data-dependency="[{&quot;action&quot;:&quot;hide&quot;,&quot;field&quot;:&quot;name&quot;,&quot;condition&quot;:&quot;equal&quot;,&quot;value&quot;:&quot;&quot;}]">Soumettre</a>
+              <input type="submit" value="submit" class="u-form-control-hidden">
+            </div>
+            <div class="u-form-send-message u-form-send-success"> Merci ! Votre message a été envoyé. </div>
+            <div class="u-form-send-error u-form-send-message"> Impossible d'envoyer votre message. Merci de corriger les erreurs et réessayer. </div>
+            <input type="hidden" value="" name="recaptchaResponse">
+            <input type="hidden" name="formServices" value="32be35ddd961770b10ee551262a9adc8">
           </form>
         </div>
-       
-        
+        <a href="Page-2.html#sec-864e" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-2">CONNEXION<br>
+        </a>
+        <h3 class="u-text u-text-default u-text-2">PAS DE COMPTE ?</h3>
+        <a href="https://nicepage.com/c/sale-website-templates" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-3">s'inscrire ?</a>
       </div>
     </section>
     
@@ -105,7 +62,15 @@ if(isset($_POST['connexion'])){
         <p class="u-small-text u-text u-text-variant u-text-1">MONOSTREET 2023 TOUT DROITS RESERVES</p>
       </div></footer>
     <section class="u-backlink u-clearfix u-grey-80">
-      
+      <a class="u-link" href="https://nicepage.com/website-design" target="_blank">
+        <span>Website Design</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="" target="_blank">
+        <span>Website Builder Software</span>
+      </a>. 
     </section>
   
 </body></html>
