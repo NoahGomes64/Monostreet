64ba61023741c839c3cdccaf2e3593ed38ab058b
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-02-06 17:24:12 +0100

    Correction d'un bug

--- pageConnexion/connexion.php
+++ pageConnexion/connexion.php
@@ -56,7 +56,7 @@ if(isset($_POST['connexion'])){
     <meta charset="utf-8">
     <meta name="keywords" content="">
     <meta name="description" content="">
-    <title>Page 2</title>
+    <title>CONNEXION</title>
     <link rel="stylesheet" href="nicepage.css" media="screen">
 <link rel="stylesheet" href="Page-2.css" media="screen">
     <script class="u-script" type="text/javascript" src="jquery-1.9.1.min.js" defer=""></script>
