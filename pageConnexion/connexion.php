42470cc542626958769ce9997c644608f1673d16
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-02-06 17:22:31 +0100

    Correction d'un bug

--- pageConnexion/connexion.php
+++ pageConnexion/connexion.php
@@ -105,8 +105,6 @@ if(isset($_POST['connexion'])){
     <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-98d6"><div class="u-clearfix u-sheet u-sheet-1">
         <p class="u-small-text u-text u-text-variant u-text-1">MONOSTREET 2023 TOUT DROITS RESERVES</p>
       </div></footer>
-    <section class="u-backlink u-clearfix u-grey-80">
-       
-    </section>
+    
   
 </body></html>
