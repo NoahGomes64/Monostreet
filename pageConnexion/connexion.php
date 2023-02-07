5abd6d7f758e41edd239e7cffe46fed289aa1ee8
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-02-07 08:54:50 +0100

    Correction d'un gros bug d'affichage et refonte de la version mobile sur la page de connexion

--- pageConnexion/connexion.php
+++ pageConnexion/connexion.php
@@ -92,6 +92,11 @@ if(isset($_POST['connexion'])){
               <label for="email-1eed" class="u-label u-label-2">MOT DE PASSE</label>
               <input type="password" placeholder="Saisir votre mot de passe" id="password" name="mdp" class="u-input u-input-rectangle u-radius-46 u-white u-input-2" required="required">
             </div>
+            <style>
+              button{
+                padding-top : 10px;
+              }
+            </style>
             <button type="submit" name="connexion" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-2">CONNEXION</button>
           </form>
         </div>
