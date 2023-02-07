bc6f566e41194f96869b6d26f54d8692b6c5746f
Noah Gomes <99337135+NoahGomes64@users.noreply.github.com>
2023-02-07 10:22:52 +0100

    Correction d'un bug

--- pageProfil/profil.php
+++ pageProfil/profil.php
@@ -73,11 +73,5 @@ background-color: #000000 !important
     
     <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-98d6"><div class="u-clearfix u-sheet u-sheet-1">
         <p class="u-small-text u-text u-text-variant u-text-1">MONOSTREET 2023 TOUT DROITS RESERVES</p>
-<<<<<<< HEAD
-    </div></footer>
-=======
-      </div></footer>
-    
-  
->>>>>>> NG1
+</div></footer>
 </body></html>
