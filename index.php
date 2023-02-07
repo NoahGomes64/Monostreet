a5cd64a5ade2bc9ae80de3b0c26fcd768fa800e7
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-02-07 10:12:48 +0100

    Mise à jour majeure du site avec notamment une toute nouvelle page profil et un header scrollable dans la page principal

--- index.php
+++ index.php
@@ -9,8 +9,6 @@
  * */
 session_start();
 ?>
-
-<!DOCTYPE html>
 <html style="font-size: 16px;" lang="fr"><head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
@@ -21,21 +19,22 @@ session_start();
 <link rel="stylesheet" href="Accueil.css" media="screen">
     <script class="u-script" type="text/javascript" src="jquery-1.9.1.min.js" defer=""></script>
     <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
-    <meta name="generator" content="Nicepage 5.4.4, nicepage.com">
+    <meta name="generator" content="Nicepage 5.4.6, nicepage.com">
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
     
     
     <script type="application/ld+json">{
 		"@context": "http://schema.org",
 		"@type": "Organization",
-		"name": "",
+		"name": "WebSite3965907",
 		"logo": "images/logo.PNG"
 }</script>
     <meta name="theme-color" content="#478ac9">
     <meta property="og:title" content="Accueil">
+    <meta property="og:description" content="">
     <meta property="og:type" content="website">
   <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
-  <body data-home-page="https://website3965907.nicepage.io/Accueil.html?version=3d389042-7c87-49cf-92b2-852b18818a59" data-home-page-title="Accueil" class="u-body u-xl-mode" data-lang="fr"><header class="u-black u-clearfix u-header u-header" id="sec-e5d6"><div class="u-clearfix u-sheet u-sheet-1">
+  <body class="u-body u-xl-mode" data-lang="fr"><header class="u-black u-clearfix u-header u-sticky u-header" id="sec-e5d6"><div class="u-clearfix u-sheet u-sheet-1">
         <a href="" class="u-image u-logo u-image-1" data-image-width="600" data-image-height="600">
           <img src="images/logo.PNG" class="u-logo-image u-logo-image-1">
         </a>
@@ -49,8 +48,9 @@ session_start();
                   </a>";
                 }
             ?>
-        
-      </div></header>
+      </div><style class="u-sticky-style" data-style-id="1613">.u-sticky-fixed.u-sticky-1613:before, .u-body.u-sticky-fixed .u-sticky-1613:before {
+borders: top right bottom left !important; border-color: #404040 !important; border-width: 2px !important
+}</style></header>
     <section class="u-align-center u-clearfix u-image u-shading u-section-1" src="" data-image-width="1936" data-image-height="1288" id="sec-dd21">
       <div class="u-clearfix u-sheet u-sheet-1">
         <h1 class="u-text u-text-default u-title u-text-1">MONOSTREET</h1>
@@ -73,6 +73,4 @@ session_start();
     <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-98d6"><div class="u-clearfix u-sheet u-sheet-1">
         <p class="u-small-text u-text u-text-variant u-text-1">MONOSTREET 2023 TOUT DROITS RESERVES</p>
       </div></footer>
-    
-  
 </body></html>
--- pageProfil/profil.php
+++ pageProfil/profil.php
@@ -11,48 +11,69 @@ session_start();
 
 
 ?>
-<!DOCTYPE html>
 <html style="font-size: 16px;" lang="fr"><head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <meta name="keywords" content="">
     <meta name="description" content="">
-    <title>Mon Profil</title>
+    <title>Mon profil</title>
     <link rel="stylesheet" href="nicepage.css" media="screen">
 <link rel="stylesheet" href="Page-1.css" media="screen">
     <script class="u-script" type="text/javascript" src="jquery-1.9.1.min.js" defer=""></script>
     <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
-    <meta name="generator" content="Nicepage 5.4.4, nicepage.com">
+    <meta name="generator" content="Nicepage 5.4.6, nicepage.com">
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
-    
     
     
     <script type="application/ld+json">{
 		"@context": "http://schema.org",
 		"@type": "Organization",
-		"name": "WebSite3965907"
+		"name": "WebSite3965907",
+		"logo": "images/logo.PNG"
 }</script>
     <meta name="theme-color" content="#478ac9">
     <meta property="og:title" content="Page 1">
     <meta property="og:description" content="">
     <meta property="og:type" content="website">
   <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
-  <body class="u-body u-xl-mode" data-lang="fr">
-
-    <section class="u-align-left u-clearfix u-image u-shading u-section-2" src="" data-image-width="1503" data-image-height="1000" id="sec-7434">
+  <body class="u-body u-xl-mode" data-lang="fr"><header class="u-black u-clearfix u-header u-sticky u-header" id="sec-e5d6"><div class="u-clearfix u-sheet u-valign-middle-xs u-sheet-1">
+        <a href="../index.php" class="u-image u-logo u-image-1" data-image-width="600" data-image-height="600">
+          <img src="images/logo.PNG" class="u-logo-image u-logo-image-1">
+        </a>
+      </div><style class="u-sticky-style" data-style-id="1613">.u-sticky-fixed.u-sticky-1613:before, .u-body.u-sticky-fixed .u-sticky-1613:before {
+borders: top right bottom left !important; border-color: #404040 !important; border-width: 2px !important
+}</style></header>
+    <section class="u-align-left u-clearfix u-image u-shading u-section-1" src="" data-image-width="1503" data-image-height="1000" id="sec-7434">
       <div class="u-clearfix u-sheet u-sheet-1">
         <h1 class="u-text u-text-default u-title u-text-1">profil</h1>
-        <h2 class="u-text u-text-2">Bonjour&nbsp; <?php echo $_SESSION['pseudo']?></h2>
-        <a href="../deconnexion.php" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-1">deconnexion</a>
+        <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout">
+            <div class="u-layout-row">
+              <div class="u-container-style u-layout-cell u-size-43-md u-size-43-sm u-size-43-xs u-size-45-lg u-size-46-xl u-layout-cell-1">
+                <div class="u-container-layout u-valign-middle u-container-layout-1">
+                  <h2 class="u-text u-text-2">Bonjour&nbsp;<?php echo $_SESSION['pseudo']?></h2>
+                </div>
+              </div>
+              <div class="u-container-style u-layout-cell u-size-14-xl u-size-15-lg u-size-17-md u-size-17-sm u-size-17-xs u-layout-cell-2">
+                <div class="u-container-layout u-valign-middle u-container-layout-2">
+                  <a href="" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-1">modifier<br>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a href="../deconnexion.php" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-2">deconnexion</a>
       </div>
     </section>
+    <style class="u-overlap-style">.u-overlap:not(.u-sticky-scroll) .u-header {
+background-color: #000000 !important
+}</style>
     
     
     <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-98d6"><div class="u-clearfix u-sheet u-sheet-1">
         <p class="u-small-text u-text u-text-variant u-text-1">MONOSTREET 2023 TOUT DROITS RESERVES</p>
       </div></footer>
-    <section class="u-backlink u-clearfix u-grey-80">
-      
-    </section>
+    
   
 </body></html>
