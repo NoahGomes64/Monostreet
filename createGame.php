aff45c60c055d29dd252d0cabe6516690a7afe98
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-02-08 15:57:06 +0100

    Correction du createGame.php avec la bonne bdd

--- createGame.php
+++ createGame.php
@@ -7,7 +7,7 @@
  * date 
  * 
  * */
-$connection = mysqli_connect("lakartxela","garricastres_bd","garricastres_bd","garricastres_bd");
+$connection = mysqli_connect("mysql-monostreet.alwaysdata.net","298407_guillaume","monostreet64!!","monostreet_utilisateur",3306);
 
 $asciiA = 65;
 $asciiZ = 90;
