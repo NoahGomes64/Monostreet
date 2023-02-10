5ebc64143a8e7c83cc4b79fa70cf0550735794f8
Guillaume Arricastres <garricastres@iutbayonne.univ-pau.fr>
2023-02-10 11:03:19 +0100

    affichage des couleurs sur le plateau

--- rechercheDeRue/creationPlateau.php
+++ rechercheDeRue/creationPlateau.php
@@ -17,18 +17,58 @@ creerPlateau("ADOUE                     ");
 	        border: 1px solid black;
         }
     </style>
-    <canvas id="myCanvas" width="581" height="581"></canvas>
+    <canvas id="myCanvas" width="987" height="987"></canvas>
         <script>
-            window.onload = function() {
+            tabDesCouleursCases = [['red','white','red','red','white','yellow','white','yellow','yellow'],
+                                    ['pink','pink','white','pink','white','white','purple','white','purple'],
+                                    ["rgb(0,32,255)",'white',"rgb(0,32,255)",'white','white',"rgb(173,216,230)",'white',"rgb(173,216,230)","rgb(173,216,230)"],
+                                    ['green','white','green','green','white',"rgb(223,175,44)",'white',"rgb(223,175,44)","rgb(223,175,44)"]];            
+            
+                window.onload = function() {
                 var canvas = document.getElementById('myCanvas');
                 var ctx = canvas.getContext('2d');
                 
-                for (let index = 0; index < 11; index++) {
+                ctx.fillStyle = "rgb(0,0,0)";
+                ctx.fillRect(1, 1, 150, 150);
+                ctx.fillRect(1, 836, 150, 150);
+                ctx.fillRect(836, 1, 150, 150);
+                ctx.fillRect(836, 836, 150, 150);
+
+                //affichage des cases
+                for (let index = 0; index < 9; index++) {
                     ctx.fillStyle = 'blue';
-                    ctx.fillRect(10 + (51*index), 10, 50, 50);
-                    ctx.fillRect(10, 10 + (51*index), 50, 50);
-                    ctx.fillRect(10 + (51*index), 561, 50, 50);
-                    ctx.fillRect(10, 10 + (51*index), 50, 50);
+                    ctx.fillRect(152 + (index *76), 1, 75, 150);
+                    ctx.fillRect(152 + (index *76), 836, 75, 150);
+                    ctx.fillRect(836, 152 + (index *76), 150, 75);
+                    ctx.fillRect(1, 152 + (index *76), 150, 75);
+                }
+
+                //affichag des cases colorées
+                for (let index1 = 0; index1 < tabDesCouleursCases.length; index1++) {
+                    for (let index2 = 0; index2 < tabDesCouleursCases[index1].length; index2++) {
+                        switch (index1) {
+                            case 1:
+                                ctx.fillStyle = tabDesCouleursCases[index1][index2];
+                                ctx.fillRect(101, 152 + (index2 *76), 50, 75);
+                                break;
+                        
+                            case 2:
+                                ctx.fillStyle = tabDesCouleursCases[index1][index2];
+                                ctx.fillRect(836, 152 + (index2 *76), 50, 75);
+                                break;
+
+                            case 3:
+                                ctx.fillStyle = tabDesCouleursCases[index1][index2];
+                                ctx.fillRect(152 + (index2 *76), 836, 75, 50);
+                                break;
+                            
+                            case 4:
+                                ctx.fillStyle = tabDesCouleursCases[index1][index2];
+                                ctx.fillRect(152 + (index2 *76), 101, 75, 50);
+                                break;
+                        }
+                    }
+                    
                 }
             }
         </script>
