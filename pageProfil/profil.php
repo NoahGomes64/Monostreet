1238aa78809d18dc97ab48287dc3f97bfc26daa3
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-02-22 10:35:25 +0100

    Nouvelle page profil

--- pageProfil/profil.php
+++ pageProfil/profil.php
@@ -9,18 +9,30 @@
  * */
 session_start();
 
+require '../connexionBD.php';
+
+
+$stmt = $connection->prepare("SELECT email FROM compte WHERE nom=:nom");
+$stmt->bindParam(':nom', $_SESSION['pseudo'], PDO::PARAM_STR);
+$stmt->execute();
+$email = $stmt->fetch();
+
 
 ?>
+
+
+<!DOCTYPE html>
 <html style="font-size: 16px;" lang="fr"><head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
-    <meta name="keywords" content="">
+    <meta name="keywords" content="modification du profil">
     <meta name="description" content="">
-    <title>Mon profil</title>
+    <title>Mon Profil</title>
     <link rel="stylesheet" href="../nicepage.css" media="screen">
 <link rel="stylesheet" href="Page-1.css" media="screen">
-    
-    <meta name="generator" content="Nicepage 5.4.6, nicepage.com">
+    <script class="u-script" type="text/javascript" src="jquery-1.9.1.min.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 5.5.0, nicepage.com">
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
     
     
@@ -32,7 +44,6 @@ session_start();
 }</script>
     <meta name="theme-color" content="#478ac9">
     <meta property="og:title" content="Page 1">
-    <meta property="og:description" content="">
     <meta property="og:type" content="website">
   <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
   <body class="u-body u-xl-mode" data-lang="fr"><header class="u-black u-clearfix u-header u-sticky u-header" id="sec-e5d6"><div class="u-clearfix u-sheet u-valign-middle-xs u-sheet-1">
@@ -44,26 +55,55 @@ borders: top right bottom left !important; border-color: #404040 !important; bor
 }</style></header>
     <section class="u-align-left u-clearfix u-image u-shading u-section-1" src="" data-image-width="1503" data-image-height="1000" id="sec-7434">
       <div class="u-clearfix u-sheet u-sheet-1">
-        <h1 class="u-text u-text-default u-title u-text-1">profil</h1>
+        <h1 class="u-hover-feature u-text u-text-default u-title u-text-1">profil</h1>
         <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
           <div class="u-layout">
             <div class="u-layout-row">
-              <div class="u-container-style u-layout-cell u-size-43-md u-size-43-sm u-size-43-xs u-size-45-lg u-size-46-xl u-layout-cell-1">
-                <div class="u-container-layout u-valign-middle u-container-layout-1">
-                  <h2 class="u-text u-text-2">Bonjour&nbsp;<?php echo $_SESSION['pseudo']?></h2>
+              <div class="u-container-style u-layout-cell u-size-60 u-layout-cell-1">
+                <div class="u-container-layout u-container-layout-1"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-2">
+          <div class="u-layout">
+            <div class="u-layout-col">
+              <div class="u-size-30">
+                <div class="u-layout-col">
+                  <div class="u-container-style u-layout-cell u-size-60 u-layout-cell-2">
+                    <div class="u-container-layout u-valign-middle u-container-layout-2">
+                      <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-1" src="images/0372c92a50c4845a7b15477773da14c9588c2c010fdf267a7b6f473d421756ac9ebea27f41570fa7d99196437a98d993f41b72d3164ca4b5e91035_1280.png" alt="" data-image-width="1280" data-image-height="1280">
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div class="u-container-style u-layout-cell u-size-14-xl u-size-15-lg u-size-17-md u-size-17-sm u-size-17-xs u-layout-cell-2">
-                <div class="u-container-layout u-valign-middle u-container-layout-2">
-                  <a href="../pageModification/modification.php" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-1">modifier le profil<br>
-                  </a>
+              <div class="u-size-30">
+                <div class="u-layout-row">
+                  <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-3">
+                    <div class="u-container-layout u-container-layout-3">
+                      <h1 class="u-text u-text-default u-text-2">Bienvenue</h1>
+                      <h2 class="u-hover-feature u-text u-text-default u-text-3">Pseudo:&nbsp;<?php echo $_SESSION['pseudo']?><br>
+                      </h2>
+                      <h2 class="u-hover-feature u-text u-text-default u-text-4">Adresse Mail:&nbsp;<?php echo $email[0]?></h2>
+                    </div>
+                  </div>
+                  <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-4">
+                    <div class="u-container-layout u-container-layout-4">
+                      <a href="../pageModification/modification.php" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-1">modifier le profil</a>
+                      <a href="" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-2">historique des parties</a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <a href="../deconnexion.php" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-2">deconnexion</a>
+        <a href="https://nicepage.com/joomla-page-builder" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-3">deconnexion<br>
+        </a>
       </div>
+      
+      
+      
     </section>
     <style class="u-overlap-style">.u-overlap:not(.u-sticky-scroll) .u-header {
 background-color: #000000 !important
@@ -72,10 +112,11 @@ background-color: #000000 !important
     
     <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-98d6"><div class="u-clearfix u-sheet u-sheet-1">
         <p class="u-small-text u-text u-text-variant u-text-1">MONOSTREET 2023 TOUT DROITS RESERVES</p>
-</div></footer>
+      </div></footer>
       <section class="u-backlink u-clearfix u-grey-80">
-      <a class="u-link" href="../pageConfidentialite/confidentialite.php">
-        <span>Politique de confidentialite</span>
-      </a>
-    </section>
+        <a class="u-link" href="../pageConfidentialite/confidentialite.php">
+          <span>Politique de confidentialite</span>
+        </a>
+      </section>
+  
 </body></html>
