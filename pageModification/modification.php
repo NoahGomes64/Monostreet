9f55667e6208869271bc54c0cb9d9e798dc23ae1
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-02-28 16:51:10 +0100

    Test upload photo profil

--- pageModification/modification.php
+++ pageModification/modification.php
@@ -18,6 +18,42 @@ $email = $stmt->fetch();
 
 // si le bouton "Enregistré" est cliqué
 if(isset($_POST['enregistrer'])){
+
+  $dossier = 'upload/';
+  $fichier = basename($_FILES['avatar']['name']);
+  $taille_maxi = 100000;
+  $taille = filesize($_FILES['avatar']['tmp_name']);
+  $extensions = array('.png', '.gif', '.jpg', '.jpeg');
+  $extension = strrchr($_FILES['avatar']['name'], '.'); 
+  //Début des vérifications de sécurité...
+  if(!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
+  {
+       $erreur = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg, txt ou doc...';
+  }
+  if($taille>$taille_maxi)
+  {
+       $erreur = 'Le fichier est trop gros...';
+  }
+  if(!isset($erreur)) //S'il n'y a pas d'erreur, on upload
+  {
+       //On formate le nom du fichier ici...
+       $fichier = strtr($fichier, 
+            'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 
+            'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+       $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
+       if(move_uploaded_file($_FILES['avatar']['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+       {
+            echo 'Upload effectué avec succès !';
+       }
+       else //Sinon (la fonction renvoie FALSE).
+       {
+            echo 'Echec de l\'upload !';
+       }
+  }
+  else
+  {
+       echo $erreur;
+  }
   $stmt = $connection->prepare("SELECT id FROM compte WHERE nom=:nom");
     $stmt->bindParam(':nom', $_SESSION['pseudo'], PDO::PARAM_STR);
     $stmt->execute();
@@ -31,6 +67,8 @@ if(isset($_POST['enregistrer'])){
         $stmt->execute();
         $_SESSION['pseudo'] = $_POST['pseudo'];
         $_SESSION['mdp'] = $_POST['mdp'];
+
+
         header ('location: ../index.php');
   }
 
