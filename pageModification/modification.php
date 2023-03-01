90798f09903aca7f8bd9c897f82b309a3efa47a0
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-03-01 09:06:01 +0100

    Test

--- pageModification/modification.php
+++ pageModification/modification.php
@@ -54,6 +54,18 @@ if(isset($_POST['enregistrer'])){
   {
        echo $erreur;
   }
+
+
+$adresse="upload/"; //Adresse du dossier sans oublier le / à la fin.
+$dossier=Opendir($adresse); //Ouverture du dossier.
+while ($Fichier = readdir($dossier)) //On affiche les fichiers les uns après les autres.
+{
+     //Maintenant, on affiche les fichiers sous forme de liens vers les fichiers 
+     //(NB : Les lien sont en target="_blank" ce qui signifie qu'ils ouvrirons une nouvelle page dans votre navigateur !).
+     echo '<a href='.$adresse.$Fichier.' target="_blank">'.$Fichier.'</a><br>';
+}
+closedir($dossier);
+
   $stmt = $connection->prepare("SELECT id FROM compte WHERE nom=:nom");
     $stmt->bindParam(':nom', $_SESSION['pseudo'], PDO::PARAM_STR);
     $stmt->execute();
