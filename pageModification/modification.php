6a9e60226bd1a81aaf1ddc183ddc2d94dd98413e
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-03-01 09:19:56 +0100

    Test

--- pageModification/modification.php
+++ pageModification/modification.php
@@ -52,7 +52,7 @@ if(isset($_POST['enregistrer'])){
     
         
         $stmt = $connection->prepare("UPDATE compte SET photo= :photo WHERE id=$id[0]");
-        $stmt->bindParam(':nom', $_FILES['avatar']['tmp_name'], PDO::PARAM_STR);
+        $stmt->bindParam(':photo', $_FILES['avatar']['tmp_name'], PDO::PARAM_STR);
         $stmt->execute();
 
        }
