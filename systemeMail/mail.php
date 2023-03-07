dbd6cca2a73c672cba46591308d42e93fc6f262f
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-03-07 15:47:21 +0100

    Ajout d'un systeme de mail pour valider les inscriptions

---
+++ systemeMail/mail.php
@@ -0,0 +1,38 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+
+$mail = new PHPMailer(True);
+
+$mail->isSMTP(); // Paramétrer le Mailer pour utiliser SMTP 
+$mail->Host = 'smtp.gmail.com'; // Spécifier le serveur SMTP
+$mail->SMTPAuth = true; // Activer authentication SMTP
+$mail->Username = 'monostreetGame@gmail.com'; // Votre adresse email d'envoi
+$mail->Password = 'monostreet64!!'; // Le mot de passe de cette adresse email
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Accepter SSL
+$mail->Port = 465;
+
+$mail->setFrom('monostreetGame@gmail.com', 'Mailer'); // Personnaliser l'envoyeur
+$mail->addAddress('noah.gomes47@gmail.com', 'Karim User'); // Ajouter le destinataire
+$mail->addAddress('To2@example.com'); 
+$mail->addReplyTo('info@example.com', 'Information'); // L'adresse de réponse
+$mail->addCC('cc@example.com');
+$mail->addBCC('bcc@example.com');
+
+
+$mail->isHTML(true); // Paramétrer le format des emails en HTML ou non
+
+$mail->Subject = 'Here is the subject';
+$mail->Body = 'This is the HTML message body';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Erreur, message non envoyé.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+ } else {
+    echo 'Le message a bien été envoyé !';
+ }
