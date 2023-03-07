bee55483c6b674b4f23bcbe020143f80a66966b7
Maxime Segot <max.segot@gmail.com>
2023-03-07 11:40:32 +0100

    Premiere classe Joueur et Pion pour le déroulement du jeu

--- JeuMonostreet/pion.php
+++ JeuMonostreet/pion.php
@@ -1,19 +1,19 @@
 <?php
 class Pion {
     private $_position;
-    private $_couleur;
+    private Couleur $_couleur;
 
     public function setPosition($nvPosition){
-        $this->$_position = $nvPosition;
+        $this->_position = $nvPosition;
     }
     public function getPosition(){
-        
+        return $this->_position;
     }
-    public function setCouleur(){
-        
+    public function setCouleur(Couleur $nvCouleur){
+        $this->_couleur = $nvCouleur;
     }
     public function getCouleur(){
-        
+        return $this->_couleur;
     }
     public function avancer(){
 
