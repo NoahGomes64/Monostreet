11fddf418681a2744e90fc91001b75d199230161
Guillaume Arricastres <garricastres@iutbayonne.univ-pau.fr>
2023-03-07 11:21:34 +0100

    affichage du plateau

--- rechercheDeRue/creationPlateau.php
+++ rechercheDeRue/creationPlateau.php
@@ -28,9 +28,9 @@ $listeRues = creerPlateau("ADOUE                     ");
 	        border: 1px solid black;
         }
     </style>
-    <!--<canvas id="myCanvas" width="987" height="987"></canvas>-->
+    <canvas id="myCanvas" width="987" height="987"></canvas>
         <script>
-            /**tabDesCouleursCases = [['red','white','red','red','white','yellow','white','yellow','yellow'],
+            tabDesCouleursCases = [['red','white','red','red','white','yellow','white','yellow','yellow'],
                                     ['pink','pink','white','pink','white','white','purple','white','purple'],
                                     ["rgb(0,32,255)",'white',"rgb(0,32,255)",'white','white',"rgb(173,216,230)",'white',"rgb(173,216,230)","rgb(173,216,230)"],
                                     ['green','white','green','green','white',"rgb(223,175,44)",'white',"rgb(223,175,44)","rgb(223,175,44)"]];            
@@ -57,44 +57,98 @@ $listeRues = creerPlateau("ADOUE                     ");
                     ctx.fillRect(1, 152 + (index *76), 150, 75);
                 }
 
-                //affichag des cases colorées
-                for (let index1 = 0; index1 < tabDesCouleursCases.length; index1++) {
+                //Liste des noms de rues
+                var x = "<?php echo $listeRues; ?>";
+                let tabDeRues = [];
+                let mot = '';  
+                for (let index = 0; index < x.length; index++) {
+                    if (x[index] == "," || index == x.lenght-1) {
+                        tabDeRues.push(mot);
+                        mot = '';
+                    } else {
+                        mot += x[index];
+                    }
+                }   
+                
+                //Suppression des espaces des rues
+                tabDeRuesSansEspace = [];
+                let mot2 = "";
+                var unCompteur = 0;
+                console.log(tabDeRues);
+                for (let index = 0; index < tabDeRues.length; index++) {
+                    tabDeRuesSansEspace.push(tabDeRues[index].trim());
+                }
+                console.log(tabDeRuesSansEspace);
+
+                //affichage des cases colorées
+                ctx.font = "30px Arial";
+                
+                var compteur = 0;
+                for (let index1 = 0; index1 < tabDeRues.length; index1++) {
                     for (let index2 = 0; index2 < tabDesCouleursCases[index1].length; index2++) {
                         switch (index1) {
                             case 0:
                                 ctx.fillStyle = tabDesCouleursCases[index1][index2];
                                 ctx.fillRect(152 + (index2 *76), 101, 75, 50);
+                                if (tabDesCouleursCases[index1][index2] != 'white') {
+                                    ctx.fillStyle = 'black';
+                                    ctx.fillText(tabDeRuesSansEspace[compteur], 152 + (index2 *76), 101, 75);
+                                    compteur += 1;
+                                }
                                 break;
                         
                             case 1:
                                 ctx.fillStyle = tabDesCouleursCases[index1][index2];
                                 ctx.fillRect(836, 152 + (index2 *76), 50, 75);
+                                if (tabDesCouleursCases[index1][index2] != 'white') {
+                                    ctx.fillStyle = 'black';
+                                    ctx.fillText(tabDeRuesSansEspace[compteur], 886, 227 + (index2 *76), 100);
+                                    compteur += 1;
+                                }
                                 break;
 
                             case 2:
                                 ctx.fillStyle = tabDesCouleursCases[index1][index2];
                                 ctx.fillRect(760 - (index2 *76), 836, 75, 50);
+                                if (tabDesCouleursCases[index1][index2] != 'white') {
+                                    ctx.fillStyle = 'black';
+                                    ctx.fillText(tabDeRuesSansEspace[compteur], 760 - (index2 *76), 907, 75);
+                                    compteur += 1;
+                                }
                                 break;
                             
                             case 3:
                                 ctx.fillStyle = tabDesCouleursCases[index1][index2];
                                 ctx.fillRect(101, 760 - (index2 *76), 50, 75);
+                                if (tabDesCouleursCases[index1][index2] != 'white') {
+                                    ctx.fillStyle = 'black';
+                                    ctx.fillText(tabDeRuesSansEspace[compteur], 1, 835 - (index2 *76), 100);
+                                    compteur += 1;
+                                }
                                 break;
                         }
                     }
-                    
                 }
 
-
+                var prison = new Image();
+                prison.src = "prison.jpg";
+                ctx.drawImage(prison, 0, 0); 
 
                 
 
-                ctx.fillStyle = "rgb(0,20,180)";
-                ctx.strokeText("Exemple de texte", 10, 20);
-            }*/
+                //ctx.font = "30px Verdana";
+                // Create gradient
+                //var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
+                //gradient.addColorStop("0"," magenta");
+                //gradient.addColorStop("0.5", "blue");
+                //gradient.addColorStop("1.0", "red");
+                // Fill with gradient
+                //ctx.fillStyle = gradient;
+                //ctx.fillText("Big smile!", 10, 90);
 
-            var x = "<?php echo $listeRues; ?>";
-            document.write(x);
+            }
+
+
             
 
  
