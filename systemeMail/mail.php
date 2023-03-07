6d089d1d2874c1cc63a03727f985a69f61703326
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-03-07 16:01:40 +0100

    Ajout d'un systeme de mail pour valider les inscriptions

--- systemeMail/mail.php
+++ systemeMail/mail.php
@@ -20,9 +20,7 @@ require 'PHPMailer/src/SMTP.php';
 
 
 //Import PHPMailer classes into the global namespace
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+
 
 require_once 'vendor/autoload.php';
 
