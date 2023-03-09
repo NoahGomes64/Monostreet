baaae8f59c631b21e3f0f0d4971301bfd1dc54bf
Jean Jonathan <jeanjonathank2@gmail.com>
2023-03-09 09:59:57 +0100

    ajout fonction co pour test co inj

--- createGame.php
+++ createGame.php
@@ -9,6 +9,20 @@
  * */
 require 'connexionBD.php';
 //Verification de la onnexion a la bd
+function connexionBD(){
+    $host = "mysql-monostreet.alwaysdata.net";
+    $dbname = "monostreet_utilisateur";
+    $username = "298407_guillaume";
+    $password = "monostreet64!!";
+
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        echo "Erreur de connexion à la base de données : " . $e->getMessage();
+    }
+}
 $pdo = connexionBD();
 
 $asciiA = 65;
