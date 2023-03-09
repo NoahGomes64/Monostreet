96b2e8271babe6b3c10b1515a18be45d33055fa5
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-03-09 10:20:07 +0100

    Correction d'un bug avec le systeme de mail

--- systemeMail/mail.php
+++ systemeMail/mail.php
@@ -119,16 +119,16 @@ mso-hide:all;
 <td align="center" style="padding:0;Margin:0"><h1 style="Margin:0;line-height:46px;mso-line-height-rule:exactly;font-family:Poppins, sans-serif;font-size:38px;font-style:normal;font-weight:bold;color:#5d541d">Veuillez confirmer votre adresse mail</h1></td>
 </tr>
 <tr>
-<td align="center" style="padding:0;Margin:0;padding-top:40px;padding-bottom:40px"><h3 style="Margin:0;line-height:24px;mso-line-height-rule:exactly;font-family:Poppins, sans-serif;font-size:20px;font-style:normal;font-weight:bold;color:#5D541D">Bienvenue dans la famille Monostreet !</h3><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:Poppins, sans-serif;line-height:27px;color:#5D541D;font-size:18px"><br></p><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:Poppins, sans-serif;line-height:27px;color:#5D541D;font-size:18px"> Pour terminer votre inscription, veuillez confirmer votre adresse e-mail. Cela garantit que nous avons le bon e-mail pour pouvoir vous contacter et assurer que vous n'êtes pas un robot.</p></td>
+<td align="center" style="padding:0;Margin:0;padding-top:40px;padding-bottom:40px"><h3 style="Margin:0;line-height:24px;mso-line-height-rule:exactly;font-family:Poppins, sans-serif;font-size:20px;font-style:normal;font-weight:bold;color:#5D541D">Bienvenue dans la famille Monostreet !</h3><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:Poppins, sans-serif;line-height:27px;color:#5D541D;font-size:18px"><br></p><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:Poppins, sans-serif;line-height:27px;color:#5D541D;font-size:18px"> Pour terminer votre inscription, veuillez confirmer votre adresse e-mail. Cela garantit que nous avons le bon e-mail pour pouvoir vous contacter et assurer que vous n êtes pas un robot.</p></td>
 </tr>
 <tr>
 <td align="center" style="padding:0;Margin:0"><!--[if mso]><a href="https://viewstripo.email" target="_blank" hidden>
 <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" esdevVmlButton href="https://viewstripo.email"
 style="height:49px; v-text-anchor:middle; width:265px" arcsize="50%" stroke="f" fillcolor="#8928c6">
 <w:anchorlock></w:anchorlock>
-<center style='color:#ffffff; font-family:Poppins, sans-serif; font-size:16px; font-weight:400; line-height:16px; mso-text-raise:1px'>Confirmer l'adresse mail</center>
+<center style="color:#ffffff; font-family:Poppins, sans-serif; font-size:16px; font-weight:400; line-height:16px; mso-text-raise:1px">Confirmer l adresse mail</center>
 </v:roundrect></a>
-<![endif]--><!--[if !mso]><!-- --><span class="msohide es-button-border" style="border-style:solid;border-color:#2CB543;background:#8928c6;border-width:0px;display:inline-block;border-radius:30px;width:auto;mso-hide:all"><a href="https://viewstripo.email" class="es-button" target="_blank" style="mso-style-priority:100 !important;text-decoration:none;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;color:#FFFFFF;font-size:16px;display:inline-block;background:#8928c6;border-radius:30px;font-family:Poppins, sans-serif;font-weight:normal;font-style:normal;line-height:19px;width:auto;text-align:center;padding:15px 35px 15px 35px;border-color:#8928c6">Confirmer l'adresse mail</a></span><!--<![endif]--></td>
+<![endif]--><!--[if !mso]><!-- --><span class="msohide es-button-border" style="border-style:solid;border-color:#2CB543;background:#8928c6;border-width:0px;display:inline-block;border-radius:30px;width:auto;mso-hide:all"><a href="https://viewstripo.email" class="es-button" target="_blank" style="mso-style-priority:100 !important;text-decoration:none;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;color:#FFFFFF;font-size:16px;display:inline-block;background:#8928c6;border-radius:30px;font-family:Poppins, sans-serif;font-weight:normal;font-style:normal;line-height:19px;width:auto;text-align:center;padding:15px 35px 15px 35px;border-color:#8928c6">Confirmer l adresse mail</a></span><!--<![endif]--></td>
 </tr>
 </table></td>
 </tr>
@@ -141,7 +141,7 @@ style="height:49px; v-text-anchor:middle; width:265px" arcsize="50%" stroke="f" 
 <td align="left" style="padding:0;Margin:0;width:470px">
 <table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
 <tr>
-<td align="center" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:Poppins, sans-serif;line-height:21px;color:#5D541D;font-size:14px">MERCI !<br>L'EQUIPE MONOSTREET</p></td>
+<td align="center" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:Poppins, sans-serif;line-height:21px;color:#5D541D;font-size:14px">MERCI !<br>L EQUIPE MONOSTREET</p></td>
 </tr>
 </table></td>
 </tr>
@@ -153,7 +153,7 @@ style="height:49px; v-text-anchor:middle; width:265px" arcsize="50%" stroke="f" 
 
 </div>
 </body>
-</html>'
+</html>';
 
 $mail = new PHPMailer(True);
 
@@ -173,7 +173,7 @@ $$mail->AltBody    = "corps du message au format texte"; //Body au format texte
 $mail->WordWrap   = 50; // nombre de caractere pour le retour a la ligne automatique
 $mail->MsgHTML($body);
                                       
-$mail->AddReplyTo("monostreet@alwaysdata.net","Monostreet");<br>       //$mail->AddAttachment("./examples/images/phpmailer.gif");             // piéce jointe si besoin
+$mail->AddReplyTo("monostreet@alwaysdata.net","Monostreet");      //$mail->AddAttachment("./examples/images/phpmailer.gif");             // piéce jointe si besoin
 $mail->AddAddress("noah.gomes47@gmail.com");
 $mail->IsHTML(true); // envoyer au format html, passer a false si en mode texte
 if(!$mail->Send()) {
