1fd49b8d432fdf2e98f52ca5d91f17821beda0b5
Maxime Segot <max.segot@gmail.com>
2023-03-10 11:52:20 +0100

    Ecriture des cartes chances

--- JeuMonostreet/Carte.php
+++ JeuMonostreet/Carte.php
@@ -12,7 +12,7 @@ class CaseCarteChance extends CasePlateau implements ICaseCarte{
         $this->initialiserListe();
     }
     public function initialiserListe(){
-
+    
     "Rendez-vous à la Rue de la Paix"
     "Avancer jusqu’à la case départ"
     "Rendez-vous à l’Avenue Henri-Martin. Si vous passez par la case départ, recevez F20 00"
@@ -37,8 +37,13 @@ class CaseCarteChance extends CasePlateau implements ICaseCarte{
 class CaseCarteCommunaute extends CasePlateau implements ICaseCarte{
     private carteCommunaute $listeCarte=[];
     public function initialiserListe(){
-        $newCarte = new carteCommunaute("Placez-vous sur la case départ",100, $leJoueur = new Joueur());
         
+        for ($i=0; $i < 16; $i++) { 
+            # code...
+        }
+
+        $newCarte = new carteCommunaute("Placez-vous sur la case départ",100, $leJoueur = new Joueur());
+        $list=[
         "Erreur de la banque en votre faveur. Recevez F20 00"
         "Payez la note du médecin F5 00"
         "La vente de votre stock vous rapporte F5 00"
@@ -46,14 +51,14 @@ class CaseCarteCommunaute extends CasePlateau implements ICaseCarte{
         "Aller en prison. Rendez-vous directement à la prison. Ne franchissez pas par la case départ, ne touchez pas F20 00"
         "Retournez à Belleville"
         "Recevez votre revenu annuel F 10 00"
-        "C’est votre anniversaire. Chaque joueur doit vous donner F 1 00"
-        "Les contributions vous remboursent la somme de F2 00"
-        "Recevez votre intérêt sur l’emprunt à 7% F2 50"
-        "Payez votre Police d’Assurance F5 00"
-        "Payez une amende de F 1 00 ou bien tirez une carte « CHANCE »"
-        "Rendez-vous à la gare la plus proche. Si vous passez par la case départ, recevez F20 00"
-        "Vous avez gagné le deuxième Prix de Beauté. Recevez F1 00"
-        "Vous héritez F10 00"
+        "C’est votre anniversaire. Chaque joueur doit vous donner F 1 00",
+        "Les contributions vous remboursent la somme de F2 00",
+        "Recevez votre intérêt sur l’emprunt à 7% F2 50",
+        "Payez votre Police d’Assurance F5 00",
+        "Payez une amende de F 1 00 ou bien tirez une carte « CHANCE »",
+        "Rendez-vous à la gare la plus proche. Si vous passez par la case départ, recevez F20 00",
+        "Vous avez gagné le deuxième Prix de Beauté. Recevez F1 00",
+        "Vous héritez F10 00"]
     }
     public function videListe(){
 
