354a86c2e219218f111a0892062eba80d3ca1fba
Jean Jonathan <jeanjonathank2@gmail.com>
2023-03-12 08:30:56 +0100

    Ajout du mini corp du fichier admin

---
+++ administrateur.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Administrateur</title>
+</head>
+<body>
+<h1>Bienvenue Administrateur : <?php echo $nom; ?></h1>
+<p>Vous êtes connecté en tant qu'administrateur. Vous avez accès à certaines fonctionnalités réservées aux administrateurs.</p>
+</body>
+</html>
