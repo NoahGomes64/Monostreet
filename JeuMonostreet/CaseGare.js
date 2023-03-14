d40a8b084139a0b99c65c4356968ae3b955e6780
Guillaume Arricastres <garricastres@iutbayonne.univ-pau.fr>
2023-03-14 12:33:13 +0100

    transformation des classes php en javascript

---
+++ JeuMonostreet/CaseGare.js
@@ -0,0 +1,49 @@
+import {Case} from './Case.js'
+
+export default class CaseGare extends Case {
+    //Constructor
+    constructor(numeroCase, nomCase) {
+        super(numeroCase, nomCase, 200);
+        this.niveauAmelioration = 0;
+        this.valeurRevenue = 25;
+        this.valeurPropriete = this.valeurAchat;
+    }
+
+    //Getter
+    get NiveauAmelioration(){
+        return this.niveauAmelioration;
+    }
+
+    get ValeurRevenue(){
+        return this.valeurRevenue;
+    }
+
+    get ValeurPropriete(){
+        return this.valeurPropriete;
+    }
+
+    //Setter
+    set NiveauAmelioration(nvNiveauAmelioration){
+        this.niveauAmelioration = nvNiveauAmelioration;
+    }
+
+    set ValeurRevenue(nvValeurRevenue){
+        this.valeurRevenue = nvValeurRevenue;
+    }
+
+    set ValeurPropriete(nvValeurPropriete){
+        this.valeurPropriete = nvValeurPropriete;
+    }
+
+    //Méthodes
+    mettreAJourRevenue(nbGare){
+        this.valeurRevenue *= nbGare;
+    }
+
+    calculerValeur(){
+        return this.prixAchat*(7/10);
+    }
+
+}
+
+export {CaseGare}
---
+++ JeuMonostreet/CaseRue.js
@@ -0,0 +1,51 @@
+import {Case} from './Case.js'
+
+export default class CaseRue extends Case {
+    //Constructor
+    constructor(numeroCase, nomCase, couleur) {
+        super(numeroCase, nomCase, (40 + 10*numeroCase));
+        this.couleur = couleur;
+        this.prixMaison = numeroCase*(3/5);
+        this.niveauAmelioration = 0;
+        this.valeurPropriete = prixAchat;
+    }
+
+    //Getter
+    get Couleur(){
+        return this.numeroCase;
+    }
+
+    get PrixMaison(){
+        return this.prixMaison;
+    }
+
+    get NiveauAmelioration(){
+        return this.niveauAmelioration;
+    }
+
+    get ValeurPropriete(){
+        return this.valeurPropriete;
+    }
+
+    //Setter
+    set Couleur(nvCouleur){
+        this.couleur = nvCouleur;
+    }
+
+    set PrixMaison(nvPrixMaison){
+        this.prixMaison = nvPrixMaison;
+    }
+
+    set NiveauAmelioration(nvNiveauAmelioration){
+        this.niveauAmelioration = nvNiveauAmelioration;
+    }
+
+    set ValeurPropriete(nvValeurPropriete){
+        this.valeurPropriete = nvValeurPropriete;
+    }
+
+    //Méthodes
+
+}
+
+export {CaseGare}
