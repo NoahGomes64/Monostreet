4eb77ef7b0033a8871f13ed0dbdf9646f74345d8
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-03-14 15:02:44 +0100

    Intégration de la page d'accueil admin

--- pageGestion/gestion.php
+++ pageGestion/gestion.php
@@ -24,7 +24,7 @@
     <meta property="og:type" content="website">
   <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
   <body class="u-body u-xl-mode" data-lang="fr"><header class="u-black u-clearfix u-header u-sticky u-header" id="sec-e5d6"><div class="u-clearfix u-sheet u-valign-middle-xs u-sheet-1">
-        <a href="https://nicepage.com" class="u-image u-logo u-image-1" data-image-width="600" data-image-height="600">
+        <a href="../index.php" class="u-image u-logo u-image-1" data-image-width="600" data-image-height="600">
           <img src="images/retour.png" class="u-logo-image u-logo-image-1">
         </a>
       </div><style class="u-sticky-style" data-style-id="1613">.u-sticky-fixed.u-sticky-1613:before, .u-body.u-sticky-fixed .u-sticky-1613:before {
@@ -33,19 +33,19 @@ borders: top right bottom left !important; border-color: #404040 !important; bor
     <section class="u-align-left u-clearfix u-image u-shading u-section-1" src="" data-image-width="1503" data-image-height="1000" id="sec-7434">
       <div class="u-clearfix u-sheet u-sheet-1">
         <h1 class="u-hover-feature u-text u-text-default u-title u-text-1">administration</h1>
-        <h2 class="u-text u-text-default u-text-2">BIENVENUE</h2>
+        <h2 class="u-text u-text-default u-text-2">BIENVENUE  <?php echo $_SESSION['pseudo']; ?></h2>
         <h3 class="u-align-center u-text u-text-default u-text-white u-text-3">Vous êtes connecté en tant qu'administrateur. Vous avez accès à certaines fonctionnalités réservées aux administrateurs</h3>
         <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
           <div class="u-layout">
             <div class="u-layout-row">
               <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-1">
                 <div class="u-container-layout u-valign-middle-sm u-valign-middle-xs u-container-layout-1">
-                  <a href="https://nicepage.one" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-1">GESTION DES SAUVEGARDES</a>
+                  <a href="" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-1">GESTION DES SAUVEGARDES</a>
                 </div>
               </div>
               <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-2">
                 <div class="u-container-layout u-valign-middle-sm u-valign-middle-xs u-container-layout-2">
-                  <a href="https://nicepage.one" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-2">GESTION DES UTILISATEURS</a>
+                  <a href="../pageAdmin/gestion.php" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-2">GESTION DES UTILISATEURS</a>
                 </div>
               </div>
             </div>
@@ -62,18 +62,12 @@ background-color: #000000 !important
     
     
     <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-98d6"><div class="u-clearfix u-sheet u-sheet-1">
-        <p class="u-small-text u-text u-text-variant u-text-1">MONOSTREET 2023 TOUT DROITS RESERVES</p>
-      </div></footer>
+      <p class="u-small-text u-text u-text-variant u-text-1">MONOSTREET 2023 TOUS DROITS RESERVES</p>
+    </div></footer>
     <section class="u-backlink u-clearfix u-grey-80">
-      <a class="u-link" href="https://nicepage.com/html-templates" target="_blank">
-        <span>HTML Template</span>
-      </a>
-      <p class="u-text">
-        <span>created with</span>
-      </p>
-      <a class="u-link" href="https://nicepage.com/html-website-builder" target="_blank">
-        <span>HTML Website Builder</span>
-      </a>. 
-    </section>
+    <a class="u-link" href="../pageConfidentialite/confidentialite.php">
+      <span>Politique de confidentialite</span>
+    </a>
+  </section>
   
 </body></html>
