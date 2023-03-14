fc46113705a81cab28d572a8ba2e838eb1b5477b
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-03-14 12:24:33 +0100

    Integration de la page de suppression du profil par l'admin

---
+++ pageAdmin/suppression.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file deconnexion.php 
+ * @brief fichier de deconnexion de la partie
+ * @autor Guillaume Arricastre
+ * version 
+ * date 
+ * 
+ * */
+    session_start();
+    require('../connexionBD.php');
+    $stmt = $connection->prepare("DELETE FROM compte  WHERE id = :id");
+    $stmt->bindParam(':id', $_GET['id'], PDO::PARAM_STR);
+    $stmt->execute();
+
+
+
+
+    header('Location: pageAdmin/gestion.php');
+?>
