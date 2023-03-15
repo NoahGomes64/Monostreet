c5b1029ca24ce548bc6019e03aebf07076925b41
Guillaume Arricastres <garricastres@iutbayonne.univ-pau.fr>
2023-03-16 00:56:05 +0100

    modif effacer pion

--- JeuMonostreet/Pion.js
+++ JeuMonostreet/Pion.js
@@ -32,7 +32,9 @@ export default class Pion {
     }
 
     allerA(numCase){
+        this.effacer();
         this.position = numCase;
+        this.afficher();
     }
 
     allerEnPrison(){
@@ -42,16 +44,16 @@ export default class Pion {
     afficher(){
         this.ctx.fillStyle = this.couleur;
         if (this.position >= 0 && this.position <= 10) {
-            this.ctx.fillRect(930 - ((this.position)*76), 920, 20, 20);
+            this.ctx.fillRect(930 - ((this.position+1)*76), 920, 20, 20);
         }
         else if (this.position > 10 && this.position <= 20) {
-            this.ctx.fillRect(25, 920 - ((this.position-10)*75), 20, 20);
+            this.ctx.fillRect(25, 920 - ((this.position-10+1)*75), 20, 20);
         }
         else if (this.position > 20 && this.position <= 30){
-            this.ctx.fillRect(125+(this.position-20)*76, 30, 20, 20);
+            this.ctx.fillRect(125+(this.position-20+1)*76, 30, 20, 20);
         }
         else if(this.position > 30 && this.position <= 39) {
-            this.ctx.fillRect(920, 110+(this.position-30)*76, 20, 20);
+            this.ctx.fillRect(920, 110+(this.position-30+1)*76, 20, 20);
         }
         
     }
