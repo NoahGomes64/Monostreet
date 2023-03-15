c3b5db69f7df652f72705e072323f92002a0cac8
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-03-15 15:16:07 +0100

    Ajustement pour permettre le bon fonctionnement du systeme de modif par l'admin

--- jeu.php
+++ jeu.php
@@ -33,6 +33,7 @@ if (strlen($leCode) == 4) {
 ?>    
 <!DOCTYPE html>
 <html>
+
     <style>
         body{
             display: grid;
@@ -190,7 +191,8 @@ if (strlen($leCode) == 4) {
     
     </body>
 	<head>
-		<title>Monostreet</title>
+		<title>MONOSTREET | Jeu</title>
+        <link rel="shortcut icon" href="images/logo.PNG" />
 	</head>
 	<body>
 	</body>
