3ca54ed626b6d3ec375ddadf3ed780f96fc32d5f
Guillaume Arricastres <garricastres@iutbayonne.univ-pau.fr>
2023-03-15 22:15:20 +0100

    transfert fichier test sur le fichier principal jeu.php

--- jeu.php
+++ jeu.php
@@ -65,6 +65,14 @@ if (strlen($leCode) == 4) {
 	        border: 1px solid black;
         }
     </style>
+    <button id="btnJouer">Jouer</button>
+
+    <script type="module" src="JeuMonostreet/main.js"></script>
+
+
+    <button id="lancerDes"></button>
+
+    <p id="gagnant"></p>
         <script>
             tabDesCouleursCases = [['red','white','red','red','white','yellow','white','yellow','yellow'],
                                     ['pink','pink','white','pink','white','white','purple','white','purple'],
