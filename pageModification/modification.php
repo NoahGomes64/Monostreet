2ff2f0fb9f3e7e67401c23b2150e8108b303e396
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-03-22 17:53:51 +0100

    Modifcations majeures du systeme de modification du profil

--- pageModification/modification.php
+++ pageModification/modification.php
@@ -321,8 +321,8 @@ if(isset($_POST['enregistrer'])){
               }
               else{
                 mail($_POST['email'],$objetMail,$message,$entete);
-                $stmt = $connection->prepare("UPDATE compte SET validationInscription= :validationInscription WHERE id=$id");
-                $stmt->bindParam(':validationInscription', 0, PDO::PARAM_STR);
+                $stmt = $connection->prepare("UPDATE compte SET validationInscription= :valider WHERE id=$id");
+                $stmt->bindParam(':valider', 0, PDO::PARAM_STR);
                 $stmt->execute();
                 header ('location: ../index.php');
               }
