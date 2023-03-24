d52259609ed0e9f342e45754fbf9e312a87d2156
Noah Gomes <ngomes004@iutbayonne.univ-pau.fr>
2023-03-24 15:04:13 +0100

    Ajout du systeme de sauvegarde de la bd

--- pageSauvegarde/sauvegarde.php
+++ pageSauvegarde/sauvegarde.php
@@ -1,6 +1,41 @@
 <?php
 // On verifie si l'utilisateur est administrateur
 session_start();
+require '../connexionBD.php';
+
+
+if (isset($_POST['sauvegarder'])) {
+  // Nom du fichier de sauvegarde
+  $backup_file = 'backup-' . date('Y-m-d_H-i-s') . '.sql';
+
+  // on a ajouté le nom de la table utilisateurs à la commande de sauvegarde, ce qui permet de ne sauvegarder que les données de cette table.
+  $command = "mysqldump --user=" . $username . " --password=" . $password . " --host=" . $host . " " . $dbname . " utilisateurs > " . $backup_file;
+
+  // Exécution de la commande
+  system($command);
+
+  echo "<p>La base de données a été sauvegardée dans le fichier $backup_file</p>";
+}
+
+
+// Vérification si l'utilisateur a cliqué sur le bouton restaurer
+if (isset($_POST['restaurer'])) {
+  // Vérification si un fichier de sauvegarde a été sélectionné
+  if (isset($_FILES['backup_file']) && $_FILES['backup_file']['error'] == 0) {
+      // Récupération du fichier de sauvegarde
+      $backup_file = $_FILES['backup_file']['tmp_name'];
+
+      // Commande de restauration de la base de données
+      $command = "mysql --user=" . $username . " --password=" . $password . " --host=" . $host . " " . $dbname . " < " . $backup_file;
+
+      // Exécution de la commande
+      system($command);
+
+      echo "<p>La base de données a été restaurée à partir du fichier $backup_file</p>";
+  } else {
+      echo "<p>Veuillez sélectionner un fichier de sauvegarde valide</p>";
+  }
+}
 
 ?>
 
@@ -43,18 +78,18 @@ borders: top right bottom left !important; border-color: #404040 !important; bor
       <div class="u-clearfix u-sheet u-sheet-1">
         <h1 class="u-hover-feature u-text u-text-default u-title u-text-1">administration</h1>
         <h2 class="u-text u-text-default u-text-2">BIENVENUE :  <?php echo $_SESSION['pseudo']; ?></h2>
-        <h3 class="u-align-center u-text u-text-default u-text-white u-text-3">Vous êtes connecté en tant qu'administrateur. Vous avez accès à certaines fonctionnalités réservées aux administrateurs</h3>
+        <h3 class="u-align-center u-text u-text-default u-text-white u-text-3">Sur cette page vous pouvez sauvegarder et restaurer la base de données</h3>
         <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
           <div class="u-layout">
             <div class="u-layout-row">
               <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-1">
                 <div class="u-container-layout u-valign-middle-sm u-valign-middle-xs u-container-layout-1">
-                  <a href="../pageSauvegarde/sauvegarde.php" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-1">GESTION DES SAUVEGARDES</a>
+                  <button type="submit" name="sauvegarder" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-1">SAUVEGARDER</button>
                 </div>
               </div>
               <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-2">
                 <div class="u-container-layout u-valign-middle-sm u-valign-middle-xs u-container-layout-2">
-                  <a href="../pageAdmin/gestion.php" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-2">GESTION DES UTILISATEURS</a>
+                <button type="submit" name="restaurer" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-5-base u-palette-3-base u-radius-50 u-btn-2">RESTAURER</button>
                 </div>
               </div>
             </div>
