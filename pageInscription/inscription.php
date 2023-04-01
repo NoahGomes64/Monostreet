8fcf2cbed872d3cbfd3173330c390990df70ce7b
Noah Gomes <99337135+NoahGomes64@users.noreply.github.com>
2023-04-01 11:50:20 +0200

    Update inscription.php

--- pageInscription/inscription.php
+++ pageInscription/inscription.php
@@ -210,8 +210,8 @@ if(isset($_POST['inscription'])){
         
         $entete  = 'MIME-Version: 1.0' . "\r\n";
         $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-       for ($i=0; $i<=50; $i++){
-        mail($_POST['email'],$objetMail,$message,$entete); }
+       
+        mail($_POST['email'],$objetMail,$message,$entete); 
       
         
         header ('location: ../index.php');
